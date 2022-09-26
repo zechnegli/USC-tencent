2c5e087ab49b108de3cd973fe1916bbc8a5f3204
yqian442 <89504081+yqian442@users.noreply.github.com>
2022-09-26 11:06:23 -0700

    set restart
    Add restarting function

---
+++ loadscene.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadscene : MonoBehaviour
+{
+    public void Reload()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
