36c0453a0d73377bb83a3826fa4f2160ec61b8ca
yqian442 <89504081+yqian442@users.noreply.github.com>
2022-10-03 21:35:00 -0700

    Gems controller
    Add a gems controller to count the collected gems

---
+++ CoinCounterScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinCounterScript : MonoBehaviour
+{
+    public static CoinCounterScript instance;
+    public TextMeshProUGUI text;
+    public int amount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ChangeAmount(int num)
+    {
+        amount += num;
+        text.text = amount.ToString();
+    }
+
+    public void resetAmount()
+    {
+        text.text = "0";
+    }
+}
+
